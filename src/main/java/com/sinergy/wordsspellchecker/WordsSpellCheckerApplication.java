be1e8009572e9d0ad561ddf95ab53d5b52d82493
Sergey Shelomentsev <sergey.shelomentsev@xored.com>
2018-11-01 21:32:16 +0700

    Initial commit

---
+++ src/main/java/com/sinergy/wordsspellchecker/WordsSpellCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.sinergy.wordsspellchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsSpellCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsSpellCheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sinergy/wordsspellchecker/WordsSpellCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sinergy.wordsspellchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordsSpellCheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
